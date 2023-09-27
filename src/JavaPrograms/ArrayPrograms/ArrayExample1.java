6dc6a0585fab8e5525a71d1e9cdae54db3a5979f
gowtham <agrss@gmail.com>
2023-09-27 12:31:52 +0530

    Spelling mistake corrected

--- src/JavaPrograms/ArrayPrograms/ArrayExample1.java
+++ src/JavaPrograms/ArrayPrograms/ArrayExample1.java
@@ -4,7 +4,7 @@ public class ArrayExample1 {
         for (int i = 0; i < arr1.length; i++) { // for ( int var : arr1);
             System.out.println(arr1[i]);
         }
-        int[] arr2 = new int[5]; //DECLRATION
+        int[] arr2 = new int[5]; //DECLARATION
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = 10;
             System.out.println(arr2[i]);
